696ef30fbd57f27a7e2e4d4704179bde424c6a87
Kevin Tayah <ktayah@yahoo.com>
2023-12-20 16:13:24 -0500

    Add specs and fix create_investor func

--- lib/belay_brokerage.ex
+++ lib/belay_brokerage.ex
@@ -1,23 +1,41 @@
 defmodule BelayBrokerage do
   @moduledoc """
-  BelayBrokerage provides an interface agaisnt the BelayBrokerage defined DB
+  BelayBrokerage provides an interface against the BelayBrokerage defined DB
   """
   alias BelayBrokerage.Investor
   alias BelayBrokerage.Repo
 
+  @type investor :: %{
+          required(:first_name) => String.t(),
+          required(:last_name) => String.t(),
+          required(:address_1) => String.t(),
+          optional(:address_2) => String.t(),
+          required(:city) => String.t(),
+          required(:region) => String.t(),
+          required(:postal_code) => String.t(),
+          required(:email) => String.t(),
+          required(:phone) => String.t()
+        }
+
+  @spec all_investors(String.t()) :: [Investor.t()]
   def all_investors(partner_id) do
     Repo.all(Investor, prefix: partner_id)
   end
 
+  @spec create_investor(String.t(), investor()) ::
+          {:ok, Investor.t()} | {:error, Ecto.Changeset.t()}
   def create_investor(partner_id, attrs) do
-    Investor.new!(attrs)
-    |> Repo.insert!(prefix: partner_id)
+    attrs
+    |> Investor.new!()
+    |> Repo.insert(prefix: partner_id)
   end
 
+  @spec get_investor(String.t(), String.t()) :: Investor.t() | nil
   def get_investor(partner_id, investor_id) do
     Repo.get(Investor, investor_id, prefix: partner_id)
   end
 
+  @spec configure_tenants_up() :: :ok
   def configure_tenants_up() do
     Ecto.Migrator.with_repo(BelayBrokerage.Repo, fn repo ->
       desired_tenants = Application.get_env(:belay_brokerage, :tenants)
@@ -32,13 +50,18 @@ defmodule BelayBrokerage do
         Ecto.Migrator.run(repo, Triplex.migrations_path(repo), :up, all: true, prefix: tenant)
       end
     end)
+
+    :ok
   end
 
+  @spec configure_tenants_down(Integer.t()) :: :ok
   def configure_tenants_down(version) do
     Ecto.Migrator.with_repo(BelayBrokerage.Repo, fn repo ->
       for tenant <- Triplex.all(repo) do
         Ecto.Migrator.run(repo, Triplex.migrations_path(repo), :down, to: version, prefix: tenant)
       end
     end)
+
+    :ok
   end
 end
